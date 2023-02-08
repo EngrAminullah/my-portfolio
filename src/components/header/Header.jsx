fe7e8dcb67d9543f7d30b5a77949123587d3b993
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2023-02-08 13:18:28 +0500

    react icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,20 +4,24 @@ import CTA from "./CTA";
 import ME from "../../assets/me.png";
 
 import "./header.css";
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Aminullah</h1>
+        <h1>John Doe</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
+        <HeaderSocial />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
 
 const HeaderSocial = () => {
   return (
@@ -7,8 +9,12 @@ const HeaderSocial = () => {
       <a href="https://linkedin.com" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank"></a>
-      <a href="https://dribble.com" target="_blank"></a>
+      <a href="https://github.com" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://dribble.com" target="_blank">
+        <FiDribbble />
+      </a>
     </div>
   );
 };
