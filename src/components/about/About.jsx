3c62a14c3760e25ca08484ad0b67dfbdca44c997
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2023-02-09 13:47:48 +0500

    responsive about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,21 +24,18 @@ const About = () => {
               <h5>Experience</h5>
               <small>3+ Years Working</small>
             </article>
-          </div>
-          <div className="about__cards">
             <article className="about__card">
               <FiUsers className="about__icon" />
-              <h5>Clients</h5>
-              <small>50+ Clients Worldwide</small>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
             </article>
-          </div>
-          <div className="about__cards">
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
-              <h5>Projects</h5>
-              <small>60+ Completed</small>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
             </article>
           </div>
+          
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta
             minus molestiae vel beatae natus eveniet ratione temporibus aperiam
