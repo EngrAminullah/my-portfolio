5c96f9733b9d7a3d836904b98c4f64a8bebdcc07
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2023-02-26 19:06:43 +0500

    Footer JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
-import Footer from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
 
 const App = () => {
   return (
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import {AiFillGithub} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {FaStackOverflow} from 'react-icons/fa'
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer>
+    <a href="#" className="footer__logo">Aminullah</a>
+
+    <ul className="permalinks">
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+
+    </ul>
+
+    <div className="footer__socials">
+      <a href="https://github.com/EngrAminullah"><AiFillGithub /></a>
+      <a href="https://www.linkedin.com/in/amin-ullah-38b9b2158/"><AiOutlineLinkedin/></a>
+      <a href="https://stackoverflow.com/users/19782229/engr-aminullah"><FaStackOverflow/></a>
+    </div>
+
+    <div className="footer__copyright">
+      <small>&copy;Aminullah. All right reserved.</small>
+    </div>
+  </footer>;
 };
 
 export default Footer;
